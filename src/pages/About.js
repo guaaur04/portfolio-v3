97e57dc27648239b9e128c9822c810f0526ece45
V Guardado <auricia.guardado@gmail.com>
2021-02-18 21:59:18 -0800

    add col comp for aboot me

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,2 +1,12 @@
 import React from "react";
 import Nav from "../components/Nav";
+import Wrapper from "./components/Wrapper";
+
+
+function About() {
+    return(
+
+    );
+}
+
+export default About; 
